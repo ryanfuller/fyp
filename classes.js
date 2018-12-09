8e5fe89f8985417e7d90e2dc2b5ff12cbcdbbf1d
Unknown <fullerryan42@gmail.com>
2018-12-09 23:24:23 +0000

    class structure set out and handling input

--- classes.js
+++ classes.js
@@ -32,7 +32,44 @@ var BarGraph = /** @class */ (function () {
     }
     return BarGraph;
 }());
+var InputManager = /** @class */ (function () {
+    function InputManager(owner) {
+        this.owner = owner;
+    }
+    InputManager.prototype.MakeNewBarGraph = function (rawInput) {
+        //check for faulty files here
+        this.owner.GraphRequest(rawInput);
+    };
+    return InputManager;
+}());
+var Grapher = /** @class */ (function () {
+    function Grapher() {
+        this.inputManager = new InputManager(this);
+        this.graphFactory = new GraphFactory(this);
+        this.graphs = new Array();
+    }
+    Grapher.prototype.GetInputManager = function () {
+        return this.inputManager;
+    };
+    Grapher.prototype.GraphRequest = function (input) {
+        alert(input);
+        this.graphs.push(this.graphFactory.MakeGraph(input));
+    };
+    return Grapher;
+}());
+var GraphFactory = /** @class */ (function () {
+    function GraphFactory(owner) {
+    }
+    GraphFactory.prototype.MakeGraph = function (input) {
+        var rows = input.split(/\r?\n/); //split the document into
+        return new BarGraph("test");
+    };
+    return GraphFactory;
+}());
+/*
 var testgraph = new BarGraph("super mega data");
-var testchannel = new BarAxis("bar 1", testgraph);
-var one = new BarChannel("one", testchannel);
-var pint = new Point(2, one);
+var testchannel = new BarAxis("bar 1",testgraph);
+var one = new BarChannel("one",testchannel);
+var pint = new Point(2,one);
+*/
+var grapher = new Grapher();
--- classes.ts
+++ classes.ts
@@ -51,8 +51,55 @@ class BarGraph implements Graph {
 	}
 }
 
+class InputManager{
+    owner : Grapher;
+	constructor (owner : Grapher){
+		this.owner=owner;
+	}
+    public MakeNewBarGraph( rawInput : string ) {
+        //check for faulty files here
+        this.owner.GraphRequest(rawInput);
+    }
+}
 
+
+
+class Grapher {
+    private inputManager = new InputManager(this);
+    private graphFactory = new GraphFactory(this);
+
+    private graphs : Array<Graph> = new Array<Graph>();
+
+    constructor() {
+
+    }
+
+
+	public GetInputManager(){
+    	return this.inputManager;
+	}
+
+	public GraphRequest(input : string){
+        alert(input);
+    	this.graphs.push(this.graphFactory.MakeGraph(input));
+	}
+}
+
+class GraphFactory{
+    constructor (owner : Grapher){
+
+    }
+    public  MakeGraph(input :string) : Graph{
+        let rows = input.split(/\r?\n/);//split the document into
+
+        return new BarGraph("test");
+    }
+}
+
+/*
 var testgraph = new BarGraph("super mega data");
 var testchannel = new BarAxis("bar 1",testgraph);
 var one = new BarChannel("one",testchannel);
 var pint = new Point(2,one);
+*/
+let grapher = new Grapher();
